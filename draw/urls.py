08900f6871b6e94e85c3151fde02e54c35c2fe98
Newman Chen <nchen1@berkeley.edu>
2021-07-23 23:39:50 -0700

    started small

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('small/', views.small, name='small'),
+    path('big/', views.big, name='big'),
     path('<str:room_name>/', views.room, name='room'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def small(request):
+    return render(request, 'draw/small.html')
+
+def big(request):
+    return render(request, 'draw/big.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
